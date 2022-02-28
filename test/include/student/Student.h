a0837ead83b27af660efd2c49e090f8fc233a0f7
Oscar Chaparro <ojcch1@gmail.com>
2022-02-28 02:06:20 -0500

    adding addresses

---
+++ test/include/student/Student.h
@@ -0,0 +1,25 @@
+//
+// Created by ojcch on 1/31/2022.
+//
+
+#include <string>
+
+#ifndef TEST_STUDENT_H
+#define TEST_STUDENT_H
+
+using namespace std;
+
+class Student {
+
+public:
+    string name;
+    string birthday;
+
+    //Student();
+
+    Student(string name, string birthday);
+
+};
+
+
+#endif //TEST_STUDENT_H
---
+++ test/src/student/Student.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ojcch on 1/31/2022.
+//
+
+#include "student/Student.h"
+
+
+
+Student::Student(string name, string birthday) {
+    this->name = name;
+    this->birthday = birthday;
+}
+
+/*Student::Student() {
+    this->name = "Undefined";
+    this->birthday = "Undefined";
+}*/
